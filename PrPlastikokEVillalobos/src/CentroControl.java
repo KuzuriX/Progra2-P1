f4a8aa37663fd25feef216446cef1fd41863ce31
Elizabeth Villalobos <villaloboselizabeth@gmail.com>
2014-11-01 17:12:25 -0600

    Mas cambios en el centro de control

--- PrPlastikokEVillalobos/src/CentroControl.java
+++ PrPlastikokEVillalobos/src/CentroControl.java
@@ -8,6 +8,7 @@ public class CentroControl {
 	private ArrayList<Operario> listaOperarios;
 	private ArrayList<Solicitud> solicitudesDespachadas;
 	private ArrayList<Solicitud> solicitudesAtendidas;
+	private final int MAX_OPERARIOS = 2;
 	
 	/**
 	 * Centro de control
@@ -25,5 +26,38 @@ public class CentroControl {
 		solicitudesDespachadas = new ArrayList<Solicitud>();
 		solicitudesAtendidas = new ArrayList<Solicitud>();
 	}
-		
+
+	/**
+	 * obtenerOperario
+	 * Retorna el operario que se encuentre en una posicion especifica del array.
+	 * @return Operario operario del centro de control
+	 */
+	private Operario obtenerOperario(int pposicion) {
+		return listaOperarios.get(pposicion);
+	}
+	
+	/**
+	 * crearOperario
+	 * Crear un nuevo operario y lo agrega a la lista de operarios del 
+	 * centro de control.
+	 * @param pcedula cedula del operario a crear.
+	 * @param pnombre nombre del operario a crear.
+	 */
+	private void crearOperario(String pcedula, String pnombre) {
+		if (listaOperarios.size() <= MAX_OPERARIOS) {
+			Operario objOperario = new Operario(pcedula, pnombre);
+			listaOperarios.add(objOperario);
+		}
+	}
+	
+	/**
+	 * despacharSolicitud
+	 * Despacha una solicitud de reparacion a un operario. Si hay mas de un
+	 * operario es necesario determinar cual tiene menos solicitudes asignadas.
+	 */
+	private void despacharSolicitud() {
+		if (listaOperarios.size() > 0) {
+			
+		}
+	}
 }
