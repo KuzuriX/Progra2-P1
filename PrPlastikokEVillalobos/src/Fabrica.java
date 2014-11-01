2ad82d8512ed2e82c64e104bf2a977dc5ed109f4
KuzuriX <cotto527@gmail.com>
2014-11-01 15:45:58 -0600

    Constructor y atributos de fabrica

--- PrPlastikokEVillalobos/src/Fabrica.java
+++ PrPlastikokEVillalobos/src/Fabrica.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 
 public class Fabrica {
+	private ArrayList<Maquina> listaMaquinas = new ArrayList<>();
+	private ObjetoControl objCentroControl;
+	
+	public Fabrica(){
+		objCentroControl = new CentroControl();
+	}
 
 }
